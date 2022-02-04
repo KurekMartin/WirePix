896ce4061e16edee39e01f4fbf46615bd8f7fc89
Martin Kurek <mar.kurek@seznam.cz>
2022-02-04 13:39:47 +0100

    Info dialog

---
+++ PhotoApp/Dialogs/AppInfoDialog.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace PhotoApp.Dialogs
+{
+    /// <summary>
+    /// Interakční logika pro ErrorDialog.xaml
+    /// </summary>
+    public partial class AppInfoDialog : UserControl
+    {
+        private MainWindow mainWindow;
+        public AppInfoDialog(MainWindow window)
+        {
+            InitializeComponent();
+            mainWindow = window;
+        }
+
+        private void btnOK_Click(object sender, RoutedEventArgs e)
+        {
+            mainWindow.DialogClose(this, null);
+        }
+    }
+}
