3bef53644564e2bd2d49fbcee236e65c5f01392b
Martin Kurek <mar.kurek@seznam.cz>
2022-02-19 19:17:32 +0100

    rewritten FileStructDialog, design changes, data binding

---
+++ PhotoApp/Converters/TagToValue.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace PhotoApp.Converters
+{
+    class TagToValue : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string tag = value as string;
+            return Tags.GetSampleValueByTag(Tags.GetTag(visibleText: tag).code);
+
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- PhotoApp/Dialogs/FileStructDialog.xaml.cs
+++ PhotoApp/Dialogs/FileStructDialog.xaml.cs
@@ -1,21 +1,47 @@
 ﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Windows.Media;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text.RegularExpressions;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
+using PhotoApp.Models;
 
 namespace PhotoApp.Dialogs
 {
-    public partial class FileStructDialog : UserControl
+    public partial class FileStructDialog : UserControl, INotifyPropertyChanged
     {
         private MainWindow mainWindow;
-        private List<string> fileStructure = new List<string>(); //struktura souboru
 
-        private int selectedTag = 0; //index vybraneho tagu
+        private ObservableCollection<string> _fileStructure = new ObservableCollection<string>(); //struktura souboru
+
+        public ObservableCollection<string> FileStructure
+        {
+            get { return _fileStructure; }
+            set { _fileStructure = value; OnPropertyChanged(); }
+        }
+
+        int _selectedIndex = -1;
+        public int SelectedIndex
+        {
+            get { return _selectedIndex; }
+            set { _selectedIndex = value; OnPropertyChanged(); }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        //private int selectedTag = 0; //index vybraneho tagu
 
         public FileStructDialog(MainWindow window, List<ButtonGroupStruct> groupList, string initStructure = "")
         {
+            DataContext = this;
             InitializeComponent();
             mainWindow = window;
 
@@ -27,14 +53,40 @@ namespace PhotoApp.Dialogs
             BaseStructDialog.FillComboBox(cbMonth, Properties.Resources.Month);
             BaseStructDialog.FillComboBox(cbDay, Properties.Resources.Day);
 
-            fileStructure = Tags.TagsToList(initStructure);
-
-            BaseStructDialog.TagsToStackPanel(spNameStruct, fileStructure, TagClick, mainWindow.mainTextStyle);
-
-            BaseStructDialog.SelectTag(spNameStruct, selectedTag, selectedTag);
-            TextBlock tb = BaseStructDialog.FindTextBlockByIndex<TextBlock>(spNameStruct, selectedTag);
-            BaseStructDialog.ShowControls(spControls, tb.Text);
+            FileStructure = new ObservableCollection<string>(Tags.TagsToList(initStructure));
+            ShowControls();
         }
+
+        private bool TagAdd(string insertValue, List<string> tags, TextBlock error)
+        {
+            if (insertValue.StartsWith("{"))
+            {
+                //omezeni maximalniho poctu tagu
+                if (tags.Count(x => x.StartsWith("{")) > 6)
+                {
+                    error.Text = "Lze použít maximálně 6 tagů";
+                    return false;
+                }
+            }
+            else
+            {
+                if (tags.Count() == 0) //tagy, které neobsahují {} (např. - _)
+                {
+                    error.Text = $"Nelze použít {Tags.GetTagByVisibleText(insertValue).visibleText} na začátku názvu.";
+                    return false;
+                }
+
+                TagStruct lastTag = Tags.GetTag(visibleText: tags.Last());
+                if (lastTag == Tags.GetTag(code: Properties.Resources.Hyphen) ||
+                    lastTag == Tags.GetTag(code: Properties.Resources.Underscore))
+                {
+                    error.Text = $"Nelze použít {insertValue} po {lastTag.visibleText}.";
+                    return false;
+                }
+            }
+            return true;
+        }
+
 
         //přidání tagu po kliknutí na tlačítko
         private void Button_Click(object sender, RoutedEventArgs e)
@@ -42,38 +94,60 @@ namespace PhotoApp.Dialogs
             tbError.Text = ""; //reset chybové hlášky
             string insertValue = ((Button)sender).Tag.ToString();
 
-            ClickReturn retVal = BaseStructDialog.AddTag(insertValue, fileStructure);
-            if (retVal.error.Length > 0)
+            if (TagAdd(insertValue, FileStructure.ToList(), tbError))
             {
-                tbError.Text = retVal.error;
-            }
-            else
-            {
-                fileStructure = retVal.tags;
+                FileStructure.Add(insertValue);
+                SelectedIndex++;
             }
 
-            BaseStructDialog.TagsToStackPanel(spNameStruct, fileStructure, TagClick, mainWindow.mainTextStyle);
-            TagsToName();
-
-            if (fileStructure.Count() > 0)
+            if (FileStructure.Count() > 0)
             {
-                //SelectTag(fileStructure.Count() - 1);
-                selectedTag = BaseStructDialog.SelectTag(spNameStruct, selectedTag, fileStructure.Count() - 1);
-                TextBlock tb = BaseStructDialog.FindTextBlockByIndex<TextBlock>(spNameStruct, selectedTag);
-                BaseStructDialog.ShowControls(spControls, tb.Text);
+                ShowControls();
             }
 
         }
 
+        private void ShowControls()
+        {
+            cbYear.Visibility = cbMonth.Visibility = cbDay.Visibility = tbCustomText.Visibility = Visibility.Collapsed;
+
+            if (SelectedIndex >= 0)
+            {
+                btnDeleteTag.Visibility = Visibility.Visible;
+                tbFileExt.Visibility = Visibility.Visible;
+
+                string tagText = FileStructure[SelectedIndex];
+                TagStruct tag = Tags.GetTag(visibleText: tagText);
+                if (tag.code.StartsWith(Properties.Resources.Year))
+                {
+                    cbYear.Visibility = Visibility.Visible;
+                }
+                else if (tag.code.StartsWith(Properties.Resources.Month))
+                {
+                    cbMonth.Visibility = Visibility.Visible;
+                }
+                else if (tag.code.StartsWith(Properties.Resources.Day))
+                {
+                    cbDay.Visibility = Visibility.Visible;
+                }
+                else if (tag.code == Properties.Resources.CustomText)
+                {
+                    tbCustomText.Visibility = Visibility.Visible;
+                    tbCustomText.Text = Tags.GetTagParameter(tagText);
+                }
+            }
+            else
+            {
+                btnDeleteTag.Visibility = Visibility.Hidden;
+                tbFileExt.Visibility = Visibility.Hidden;
+            }
+        }
+
 
         //vyber tagu v nazvu souboru
-        private void TagClick(object sender, MouseButtonEventArgs e)
+        private void TagClick(object sender, SelectionChangedEventArgs e)
         {
-            TextBlock block = sender as TextBlock;
-            int index = (int)block.Tag;
-            //SelectTag(index);
-            selectedTag = BaseStructDialog.SelectTag(spNameStruct, selectedTag, index);
-            BaseStructDialog.ShowControls(spControls, block.Text);
+            ShowControls();
         }
 
         //kontrola textBoxu - povolené jen písmena - _
@@ -90,63 +164,22 @@ namespace PhotoApp.Dialogs
             if (e.RemovedItems.Count > 0)
             {
                 ComboBoxItem cbItem = ((ComboBox)sender).SelectedItem as ComboBoxItem;
-
-                ChangeReturn cReturn = BaseStructDialog.ComboBoxChanged(cbItem, selectedTag, fileStructure);
-                fileStructure = cReturn.tagStructure;
-
-
-                BaseStructDialog.TagsToStackPanel(spNameStruct, fileStructure, TagClick, mainWindow.mainTextStyle);
-                TagsToName();
-
-                //SelectTag(index);
-                selectedTag = BaseStructDialog.SelectTag(spNameStruct, selectedTag, cReturn.index);
-                TextBlock tb = BaseStructDialog.FindTextBlockByIndex<TextBlock>(spNameStruct, selectedTag);
-                BaseStructDialog.ShowControls(spControls, tb.Text);
+                int oldIndex = SelectedIndex;
+                FileStructure[SelectedIndex] = Tags.GetTag(label: cbItem.Content.ToString()).visibleText;
+                SelectedIndex = oldIndex;
             }
         }
-
-        private void TagsToName()
-        {
-            if(fileStructure.Count == 0)
-            {
-                spFileName.Visibility = Visibility.Hidden;
-            }
-            else
-            {
-                spFileName.Visibility = Visibility.Visible;
-            }
-            //dosazení hodnot za tagy
-            string fileName = Tags.TagsToValues(fileStructure);
-            if (fileName != "")
-            {
-                fileName += ".xxx";
-            }
-            tbFileName.Text = fileName;
-        }
-
 
         //smazani vybraneho tagu
         private void btnDeleteTag_Click(object sender, RoutedEventArgs e)
         {
-            if (fileStructure.Count > 0)
+            int oldIndex = SelectedIndex;
+            if (FileStructure.Count > 0)
             {
-                fileStructure.RemoveAt(selectedTag);
+                FileStructure.RemoveAt(SelectedIndex);
+                SelectedIndex = oldIndex-1;
             }
-
-
-            BaseStructDialog.TagsToStackPanel(spNameStruct, fileStructure, TagClick, mainWindow.mainTextStyle);
-            TagsToName();
-
-            //pokud smažeme poslední tag -> vybere se opět poslední
-            if (selectedTag >= fileStructure.Count())
-            {
-                selectedTag = fileStructure.Count() - 1;
-
-            }
-            //selectedTag(selectedTag);
-            selectedTag = BaseStructDialog.SelectTag(spNameStruct, selectedTag, selectedTag);
-            TextBlock tb = BaseStructDialog.FindTextBlockByIndex<TextBlock>(spNameStruct, selectedTag);
-            BaseStructDialog.ShowControls(spControls, tb.Text);
+            ShowControls();
         }
 
 
@@ -155,22 +188,12 @@ namespace PhotoApp.Dialogs
         {
             if (e.Changes.First().AddedLength == 1 || e.Changes.First().RemovedLength == 1)
             {
-
-                int textPos = selectedTag;
-                int i = fileStructure[textPos].IndexOf("(");
+                int i = FileStructure[SelectedIndex].IndexOf("(");
                 if (i != -1)
                 {
-                    fileStructure[textPos] = fileStructure[textPos].Substring(0, i);
+                    FileStructure[SelectedIndex] = FileStructure[SelectedIndex].Substring(0, i);
                 }
-                fileStructure[textPos] += $"({tbCustomText.Text})";
-
-                BaseStructDialog.TagsToStackPanel(spNameStruct, fileStructure, TagClick, mainWindow.mainTextStyle);
-                TagsToName();
-
-                //SelectTag(textPos);
-                selectedTag = BaseStructDialog.SelectTag(spNameStruct, selectedTag, textPos);
-                TextBlock tb = BaseStructDialog.FindTextBlockByIndex<TextBlock>(spNameStruct, selectedTag);
-                BaseStructDialog.ShowControls(spControls, tb.Text);
+                FileStructure[SelectedIndex] += $"({tbCustomText.Text})";
             }
 
         }
@@ -187,19 +210,19 @@ namespace PhotoApp.Dialogs
         //ukončení formuláře
         private void btnDone_Click(object sender, RoutedEventArgs e)
         {
-            int i = BaseStructDialog.CheckCustomText(fileStructure);
-            if (i > -1)
-            {
-                selectedTag = BaseStructDialog.SelectTag(spNameStruct, selectedTag, i);
-                TextBlock tb = BaseStructDialog.FindTextBlockByIndex<TextBlock>(spNameStruct, selectedTag);
-                BaseStructDialog.ShowControls(spControls, tb.Text);
+            //int i = BaseStructDialog.CheckCustomText(FileStructure.ToList());
+            //if (i > -1)
+            //{
+            //    selectedTag = BaseStructDialog.SelectTag(spNameStruct, selectedTag, i);
+            //    TextBlock tb = BaseStructDialog.FindTextBlockByIndex<TextBlock>(spNameStruct, selectedTag);
+            //    BaseStructDialog.ShowControls(spControls, tb.Text);
 
-                tbError.Text = $"Chybí hodnota pro {Tags.GetTagByCode(Properties.Resources.CustomText).visibleText}.\n" +
-                    $"Doplňte tuto hodnotu nebo tag odstraňte";
-                tbCustomText.Focus();
-                return;
-            }
-            string result = string.Join("", fileStructure.ToArray());
+            //    tbError.Text = $"Chybí hodnota pro {Tags.GetTagByCode(Properties.Resources.CustomText).visibleText}.\n" +
+            //        $"Doplňte tuto hodnotu nebo tag odstraňte";
+            //    tbCustomText.Focus();
+            //    return;
+            //}
+            string result = string.Join("", FileStructure.ToList());
             mainWindow.DialogClose(this, result);
         }
     }
