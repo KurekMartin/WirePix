1b95ac1bdfcc63e8bacd96255dc17a553fe81a1b
Martin Kurek <mar.kurek@seznam.cz>
2022-02-24 20:11:21 +0100

    index to margin

--- PhotoApp/Converters/IndexToMargin.cs
+++ PhotoApp/Converters/IndexToMargin.cs
@@ -12,20 +12,7 @@ namespace PhotoApp.Converters
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
             Thickness margin = new Thickness(0, 0, 0, 0);
-            CollectionViewSource itemSource = parameter as CollectionViewSource;
-            var items = itemSource.Source as ObservableCollection<ObservableCollection<string>>;
-            if (items != null)
-            {
-                margin.Left = items.IndexOf(value as ObservableCollection<string>) * 10;
-            }
-            else
-            {
-                var itemsList = itemSource.Source as List<List<string>>;
-                if(itemsList != null)
-                {
-                    margin.Left = itemsList.IndexOf(value as List<string>) * 10;
-                }
-            }
+            margin.Left = System.Convert.ToDouble((int)value) * 10;
 
             return margin;
         }
