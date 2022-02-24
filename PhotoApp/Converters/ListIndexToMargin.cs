2e78c61b1d8171721276be82ebfa53896fac9d3c
Martin Kurek <mar.kurek@seznam.cz>
2022-02-24 20:15:58 +0100

    added

---
+++ PhotoApp/Converters/ListIndexToMargin.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace PhotoApp.Converters
+{
+    class ListIndexToMargin : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            Thickness margin = new Thickness(0, 0, 0, 0);
+            CollectionViewSource itemSource = parameter as CollectionViewSource;
+            var items = itemSource.Source as ObservableCollection<ObservableCollection<string>>;
+            if (items != null)
+            {
+                margin.Left = items.IndexOf(value as ObservableCollection<string>) * 10;
+            }
+            else
+            {
+                var itemsList = itemSource.Source as List<List<string>>;
+                if(itemsList != null)
+                {
+                    margin.Left = itemsList.IndexOf(value as List<string>) * 10;
+                }
+            }
+
+            return margin;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ PhotoApp/Converters/MarginFromListCount.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace PhotoApp.Converters
+{
+    class MarginFromListCount : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            Thickness margin = new Thickness(0, 0, 0, 0);
+            CollectionViewSource itemSource = parameter as CollectionViewSource;
+            var items = itemSource.Source as ObservableCollection<ObservableCollection<string>>;
+            if (items != null)
+            {
+                margin.Left = items.Count * 10;
+            }
+            else
+            {
+                var itemsList = itemSource.Source as List<List<string>>;
+                if(itemsList != null)
+                {
+                    margin.Left = itemsList.Count * 10;
+                }
+            }
+
+            return margin;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
