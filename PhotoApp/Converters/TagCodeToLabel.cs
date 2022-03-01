212fcc0dd99da45caef982638965ddd0dd09cc84
Martin Kurek <mar.kurek@seznam.cz>
2022-03-01 19:29:00 +0100

    added

---
+++ PhotoApp/Converters/TagCodeToLabel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace PhotoApp.Converters
+{
+    internal class TagCodeToLabel : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string code, tag;
+            if (value is string)
+            {
+                tag = code = value as string;
+                if (tag.Contains("("))
+                {
+                    code = Tags.RemoveParameter(tag);
+                    return $"{Tags.GetTag(code: code).VisibleText}({Tags.GetParameter(tag)})";
+                }
+                else
+                {
+                    return Tags.GetTag(code: code).VisibleText;
+                }
+            }
+            else
+            {
+                return string.Empty;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
