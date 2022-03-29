8aac1291990b29f1209b9581d660d044d9ccd39b
Martin Kurek <mar.kurek@seznam.cz>
2022-03-29 17:51:33 +0200

    added dark mode change resources now use keys

--- PhotoApp/App.xaml.cs
+++ PhotoApp/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MaterialDesignThemes.Wpf;
+using System;
 using System.Deployment.Application;
 using System.IO;
 using System.Reflection;
@@ -14,12 +15,12 @@ namespace PhotoApp
         private void Application_Startup(object sender, StartupEventArgs e)
         {
             string appData = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
-            string mainFolder = "PhotoManager";
-            Resources.Add("tmpFolder", Path.Combine(Path.GetTempPath(), mainFolder));
-            Resources.Add("logFolder", Path.Combine(appData, mainFolder, "Logs"));
-            Resources.Add("profilesFolder", Path.Combine(appData, mainFolder, "Profiles"));
-            Resources.Add("dataFolder", Path.Combine(appData, mainFolder, "Data"));
-            Resources.Add("crashFolder", Path.Combine(appData, mainFolder, "Crash Reports"));
+            string mainFolder = "WirePix";
+            Resources.Add(PhotoApp.Properties.Keys.TempFolder, Path.Combine(Path.GetTempPath(), mainFolder));
+            Resources.Add(PhotoApp.Properties.Keys.LogsFolder, Path.Combine(appData, mainFolder, "Logs"));
+            Resources.Add(PhotoApp.Properties.Keys.ProfilesFolder, Path.Combine(appData, mainFolder, "Profiles"));
+            Resources.Add(PhotoApp.Properties.Keys.DataFolder, Path.Combine(appData, mainFolder, "Data"));
+            Resources.Add(PhotoApp.Properties.Keys.CrashReportsFolder, Path.Combine(appData, mainFolder, "Crash Reports"));
 
             var keys = Current.Resources.Keys.GetEnumerator();
             while (keys.MoveNext())
@@ -29,9 +30,9 @@ namespace PhotoApp
                 {
                     Directory.CreateDirectory(Current.Resources[key].ToString());
                 }
-
             }
-            
+
+            SetThemeMode(PhotoApp.Properties.Settings.Default.DarkMode);
         }
 
         public string Version
@@ -46,6 +47,27 @@ namespace PhotoApp
                 {
                     return Assembly.GetExecutingAssembly().GetName().Version.ToString(3);
                 }
+            }
+        }
+
+        public static void SetThemeMode(bool darkMode)
+        {
+            var paletteHelper = new PaletteHelper();
+            //Retrieve the app's existing theme
+            ITheme theme = paletteHelper.GetTheme();
+            if (darkMode)
+            {
+                theme.SetBaseTheme(Theme.Dark);
+            }
+            else
+            {
+                theme.SetBaseTheme(Theme.Light);
+            }
+            paletteHelper.SetTheme(theme);
+            if (darkMode != PhotoApp.Properties.Settings.Default.DarkMode)
+            {
+                PhotoApp.Properties.Settings.Default.DarkMode = darkMode;
+                PhotoApp.Properties.Settings.Default.Save();
             }
         }
     }
