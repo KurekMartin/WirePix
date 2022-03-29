624e4892c0796698bd5da5445f5bedccf5c87567
Martin Kurek <mar.kurek@seznam.cz>
2022-03-29 17:49:53 +0200

    added settings dialog

---
+++ PhotoApp/Dialogs/AppSettingsDialog.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace PhotoApp.Dialogs
+{
+    /// <summary>
+    /// Interakční logika pro ErrorDialog.xaml
+    /// </summary>
+    public partial class AppSettingsDialog : UserControl
+    {
+        private MainWindow mainWindow;
+        public AppSettingsDialog(MainWindow window)
+        {
+            InitializeComponent();
+            mainWindow = window;
+            cbDarkMode.IsChecked = Properties.Settings.Default.DarkMode;
+        }
+
+        private void cbDarkMode_Changed(object sender, RoutedEventArgs e)
+        {
+            CheckBox checkBox = (CheckBox)sender;
+            App.SetThemeMode((bool)checkBox.IsChecked);
+        }
+        private void btnOK_Click(object sender, RoutedEventArgs e)
+        {
+            mainWindow.DialogClose(this, null);
+        }
+    }
+}
