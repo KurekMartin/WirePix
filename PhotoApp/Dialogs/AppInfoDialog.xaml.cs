c97aa6756a595f16c85750dfbe32d708ec121d91
Martin Kurek <mar.kurek@seznam.cz>
2022-03-30 19:04:50 +0200

    update function

--- PhotoApp/Dialogs/AppInfoDialog.xaml.cs
+++ PhotoApp/Dialogs/AppInfoDialog.xaml.cs
@@ -2,6 +2,11 @@
 using System;
 using System.Windows;
 using System.Windows.Controls;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.IO;
 
 namespace PhotoApp.Dialogs
 {
@@ -11,6 +16,9 @@ namespace PhotoApp.Dialogs
     public partial class AppInfoDialog : UserControl
     {
         private MainWindow mainWindow;
+        private Release newRelease;
+        private bool downloading = false;
+        private static readonly string _tmpFolder = System.Windows.Application.Current.Resources[Properties.Keys.TempFolder].ToString();
         public AppInfoDialog(MainWindow window)
         {
             InitializeComponent();
@@ -30,17 +38,19 @@ namespace PhotoApp.Dialogs
             try
             {
                 var github = new GitHubClient(new ProductHeaderValue("WirePix"));
-                var release = await github.Repository.Release.GetLatest("Bassman2", "MediaDevices"); //změnit "KurekMartin", "WirePix"
-                var latestVersion = Version.Parse(release.TagName);
-               
+                var release = await github.Repository.Release.GetLatest("KurekMartin", "WirePix");
+                var latestVersion = Version.Parse(release.TagName.Replace("v", ""));
+
                 if (latestVersion > currentVersion)
                 {
                     tbVersionInfo.Text = "Je dostupná novější verze programu";
-                    //System.Diagnostics.Process.Start(release.HtmlUrl);
+                    newRelease = release;
+                    spDownloadUpdate.Visibility = Visibility.Visible;
                 }
                 else
                 {
                     tbVersionInfo.Text = "Máte nejnovější verzi programu";
+                    spDownloadUpdate.Visibility = Visibility.Hidden;
                 }
             }
             catch
@@ -49,21 +59,32 @@ namespace PhotoApp.Dialogs
             }
             tbVersionInfo.Visibility = Visibility.Visible;
             MaterialDesignThemes.Wpf.ButtonProgressAssist.SetIsIndicatorVisible(btnCheckUpdate, false);
-
-            
-
-            //Console.WriteLine(release.TagName);
-
         }
 
-        private void btnAutoUpdate_Click(object sender, RoutedEventArgs e)
+        private async void btnAutoUpdate_Click(object sender, RoutedEventArgs e)
         {
+            if (newRelease != null && !downloading)
+            {
+                downloading = true;
+                var asset = newRelease.Assets.FirstOrDefault(a => a.Name.EndsWith(".msi"));
+                string installerPath = Path.Combine(_tmpFolder, asset.Name);
+
+                WebClient webClient = new WebClient();
+                MaterialDesignThemes.Wpf.ButtonProgressAssist.SetIsIndicatorVisible(btnAutoUpdate, true);
+                btnOK.IsEnabled = btnCheckUpdate.IsEnabled = false; //disable buttons
+                await webClient.DownloadFileTaskAsync(new Uri(asset.BrowserDownloadUrl), installerPath);
+                downloading = false;
+                MaterialDesignThemes.Wpf.ButtonProgressAssist.SetIsIndicatorVisible(btnAutoUpdate, false);
+
+                Process.Start(installerPath);
+                System.Windows.Application.Current.Shutdown();
+            }
 
         }
 
-        private void btnManualUpdate_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
+    private void btnManualUpdate_Click(object sender, RoutedEventArgs e)
+    {
+        Process.Start(newRelease.HtmlUrl);
     }
+}
 }
