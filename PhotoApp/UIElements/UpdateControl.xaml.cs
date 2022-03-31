a119834902d5426c3fae6daa41b2668e3c3e1160
Martin Kurek <mar.kurek@seznam.cz>
2022-03-31 19:52:23 +0200

    added folder for ui elements

--- PhotoApp/UIElements/UpdateControl.xaml.cs
+++ PhotoApp/UIElements/UpdateControl.xaml.cs
@@ -10,7 +10,7 @@ using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 
-namespace PhotoApp.Dialogs
+namespace PhotoApp.UIElements
 {
     public partial class UpdateControl : UserControl, INotifyPropertyChanged
     {
