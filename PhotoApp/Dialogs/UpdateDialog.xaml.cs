25721a3e4f4ee333b76677f05b11e796503665f2
Martin Kurek <mar.kurek@seznam.cz>
2022-04-08 16:56:52 +0200

    created update dialog

---
+++ PhotoApp/Dialogs/UpdateDialog.xaml.cs
@@ -0,0 +1,39 @@
+﻿using Octokit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PhotoApp.Dialogs
+{
+    /// <summary>
+    /// Interaction logic for UpdateDialog.xaml
+    /// </summary>
+    public partial class UpdateDialog : UserControl
+    {
+        private MainWindow mainWindow;
+        public UpdateDialog(MainWindow window, Release release)
+        {
+            InitializeComponent();
+            mainWindow = window;
+            var version = Version.Parse(release.TagName.Replace("v", ""));
+            ucUpdate.VersionInfo = $"Je dostupná verze {version}";
+            ucUpdate.Release = release;
+        }
+
+        private void btnOK_Click(object sender, RoutedEventArgs e)
+        {
+            mainWindow.DialogClose(this, null);
+        }
+    }
+}
