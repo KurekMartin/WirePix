371f0039b38c367c7ad784fa8ea57cf6d7196d5b
Martin Kurek <mar.kurek@seznam.cz>
2022-06-13 11:12:08 +0200

    moved file type info to separate class

---
+++ PhotoApp/FileType.cs
@@ -0,0 +1,23 @@
+﻿using ImageMagick;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhotoApp
+{
+    internal class FileType
+    {
+        public static bool IsImage(string fileName)
+        {
+            var format = MagickFormatInfo.Create(fileName);
+            return format != null && (format.ModuleFormat == MagickFormat.Dng || (format.MimeType != null && format.MimeType.Contains("image")));
+        }
+        public static bool IsVideo(string fileName)
+        {
+            var format = MagickFormatInfo.Create(fileName);
+            return format != null && (format.MimeType != null && format.MimeType.Contains("video") || format.Description.ToLower().Contains("video"));
+        }
+    }
+}
