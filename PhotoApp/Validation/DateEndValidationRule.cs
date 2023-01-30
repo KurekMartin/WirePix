0694f2bc8986bd9171bc5e11dc4e57f9987e1c36
Martin Kurek <mar.kurek@seznam.cz>
2023-01-30 14:02:44 +0100

    moved date range input added possibility to disable invalid date ranges

---
+++ PhotoApp/Validation/DateEndValidationRule.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace PhotoApp.Validation
+{
+    internal class DateEndValidationRule : ValidationRule
+    {
+        public DateRangeChecker RangeChecker { get; set; }
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            if (value == null)
+            {
+                return new ValidationResult(false, "Select date");
+            }
+
+            var startDate = (DateTime)value;
+            var endDate = RangeChecker.EndDate;
+            if (endDate > startDate)
+            {
+                return new ValidationResult(false, "");
+            }
+
+            return ValidationResult.ValidResult;
+        }
+    }
+}
---
+++ PhotoApp/Validation/DateStartValidationRule.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Controls;
+
+namespace PhotoApp.Validation
+{
+    internal class DateStartValidationRule : ValidationRule
+    {
+        public DateRangeChecker RangeChecker { get; set; }
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            if (value == null)
+            {
+                return new ValidationResult(false, "Select date");
+            }
+
+            var startDate = (DateTime)value;
+            var endDate = RangeChecker.EndDate;
+            if (endDate < startDate)
+            {
+                return new ValidationResult(false, "");
+            }
+
+            return ValidationResult.ValidResult;
+        }
+    }
+}
