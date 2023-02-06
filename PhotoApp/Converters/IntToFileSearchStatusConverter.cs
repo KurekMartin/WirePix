85903b9ae6091bcec653b12eb3ce20faf12cbb00
Martin Kurek <mar.kurek@seznam.cz>
2023-02-06 13:19:40 +0100

    changed to switch case

--- PhotoApp/Converters/IntToFileSearchStatusConverter.cs
+++ PhotoApp/Converters/IntToFileSearchStatusConverter.cs
@@ -9,39 +9,28 @@ namespace PhotoApp.Converters
         public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
         {
             var state = (int)value;
-            if (state == DeviceInfo.DEVICE_FILES_NOT_SEARCHED)
+            switch (state)
             {
-                return Properties.Resources.FileSearchStatus_NotSearched;
-            }
-            else if(state == DeviceInfo.DEVICE_FILES_WAITING)
-            {
-                return Properties.Resources.FileSearchStatus_Waiting;
-            }
-            else if (state == DeviceInfo.DEVICE_FILES_SEARCHING)
-            {
-                return Properties.Resources.FileSearchStatus_Searching;
-            }
-            else if (state == DeviceInfo.DEVICE_FILES_READY)
-            {
-                return Properties.Resources.FileSearchStatus_Ready;
-            }
-            else if(state == DeviceInfo.DEVICE_FILES_CANCELED)
-            {
-                return Properties.Resources.FileSearchStatus_Canceled;
-            }
-            else if (state == DeviceInfo.DEVICE_FILES_ERROR)
-            {
-                return Properties.Resources.FileSearchStatus_Error;
-            }
-            else
-            {
-                return Properties.Resources.FileSearchStatus_Unknown;
-            }
+                case Device.DEVICE_FILES_NOT_SEARCHED:
+                    return Properties.Resources.FileSearchStatus_NotSearched;
+                case Device.DEVICE_FILES_WAITING:
+                    return Properties.Resources.FileSearchStatus_Waiting;
+                case Device.DEVICE_FILES_SEARCHING:
+                    return Properties.Resources.FileSearchStatus_Searching;
+                case Device.DEVICE_FILES_READY:
+                    return Properties.Resources.FileSearchStatus_Ready;
+                case Device.DEVICE_FILES_CANCELED:
+                    return Properties.Resources.FileSearchStatus_Canceled;
+                case Device.DEVICE_FILES_ERROR:
+                    return Properties.Resources.FileSearchStatus_Error;
+                default:
+                    return Properties.Resources.FileSearchStatus_Unknown;
 
+            }
         }
-        public object ConvertBack(object value, Type targetTypes, object parameter, System.Globalization.CultureInfo culture)
-        {
-            throw new NotImplementedException();
+            public object ConvertBack(object value, Type targetTypes, object parameter, System.Globalization.CultureInfo culture)
+            {
+                throw new NotImplementedException();
+            }
         }
     }
-}
